ee6530fb077ed603d71fea2c49e27a0c9dc9510c
guixu40 <2929067689@qq.com>
2020-06-11 10:12:03 +0800

    user

--- src/main.js
+++ src/main.js
@@ -9,6 +9,11 @@ import axios from 'axios'
 Vue.prototype.$http = axios
 // 配置请求的跟路径
 axios.defaults.baseURL = 'https://www.liulongbin.top:8888/api/private/v1/'
+axios.interceptors.request.use(config => {
+  // console.log(config);
+  config.headers.Authorization = window.sessionStorage.getItem('token')
+  return config;
+})
 
 
 Vue.config.productionTip = false
